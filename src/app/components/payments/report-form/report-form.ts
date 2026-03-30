f0b6adad024081cdb39e7b4da7b9b286979a9eae
juan <bravo43juancho@gmail.com>
2026-03-30 16:40:00 -0300

    fix: show current date on payments view #5

--- src/app/components/payments/report-form/report-form.ts
+++ src/app/components/payments/report-form/report-form.ts
@@ -1,4 +1,4 @@
-import {Component, output, signal} from '@angular/core';
+import {Component, OnInit, output, signal} from '@angular/core';
 import {ReportQuery} from '../../../core/DTO/ReportQuery';
 import {form, FormField} from '@angular/forms/signals';
 import {FormsModule} from '@angular/forms';
@@ -12,19 +12,29 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './report-form.html',
   styleUrl: './report-form.scss',
 })
-export class ReportForm {
+export class ReportForm implements OnInit {
 
 
   query = signal<ReportQuery>({
     page:0,
     limit:10,
     type: 'ALL',
-    start:'',
-    end: '',
+    start: '',
+    end:''
   });
   queryForm = form(this.query);
 
   finalQuery = output<ReportQuery>();
+  ngOnInit() {
+
+    this.query.update(prev => {
+      return{
+        ...prev,
+        start: new Date(new Date().setHours(0, 0, 0, 0)).toISOString().split('T')[0],
+        end: new Date(new Date().setHours(23, 59, 59, 999)).toISOString().split('T')[0],
+      }    })
+
+  }
 
   submittedForm(){
     this.finalQuery.emit(this.query());
