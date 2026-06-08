4723b0d113b07b325a0d111c65e3df79e5cdd90a
juan <bravo43juancho@gmail.com>
2026-06-08 18:55:02 -0300

    feature: Enhance delete confirmation dialog UI and functionality

--- src/app/components/layout/delete-confirmation/delete-confirmation.ts
+++ src/app/components/layout/delete-confirmation/delete-confirmation.ts
@@ -1,23 +1,30 @@
 import {Component, inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef} from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import {IDeleteDialogData} from '../../../core/Interfaces/DeleteDialogData';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-delete-confirmation',
   imports: [
-    MatDialogContent,
-    MatDialogActions
+    MatIcon
   ],
   templateUrl: './delete-confirmation.html',
   styleUrl: './delete-confirmation.scss',
 })
 export class DeleteConfirmation {
-  protected data : IDeleteDialogData = inject(MAT_DIALOG_DATA)
   private dialogRef = inject(MatDialogRef<DeleteConfirmation>)
-  onCancel(): void {
+  protected data : IDeleteDialogData = inject(MAT_DIALOG_DATA)
+  protected onCancel(): void {
     this.dialogRef.close(false);
   }
-  onConfirm(): void {
+  protected onConfirm(): void {
     this.dialogRef.close(true);
   }
+
+  protected close (){
+    this.dialogRef.close(false);
+  };
 }
